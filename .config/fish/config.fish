38e3b0736aa3e7244f268983ccd587e95f353847
Kazuki Shigemichi <shigemichik@gmail.com>
2022-06-12 21:11:39 +0900

    fish: add commands related lisp

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,6 +19,26 @@ fish_add_path $HOME/go/bin
 set -x LESSOPEN '| /usr/bin/src-hilite-lesspipe.sh %s'
 set -x LESS '-RX'
 
+# https://lemniscus.hatenablog.com/entry/20100701/1277990269
+# CTRL+^: Use an external editor to edit the current input.
+function rlccl
+  set -x RLWRAP_EDITOR 'micro -filetype lisp'
+  rlwrap -c -q '"' -b "'"'(){}[].,#@;|`"' -m ccl $argv
+end
+
+# function rlgosh
+#   set -x RLWRAP_EDITOR 'micro -filetype lisp'
+#   rlwrap -c -q '"' -b "'"'(){}[].,#@;|`"' -m gosh $argv
+# end
+function rlgosh
+  set -x GAUCHE_EDITOR 'micro'
+  gosh $argv
+end
+
+function runccl
+  ccl --batch --eval '(progn (load "'$argv[1]'") (ccl::quit))'
+end
+
 function cdl
   set dirname $argv[1]
   eval "cd" $dirname
